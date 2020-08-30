ea2d0904c67f9207bc5bee7703860f64efd8c696
jose roman <el-1ero@outlook.com>
2020-08-30 19:26:41 -0400

    Updates

---
+++ precache-manifest.92abf2c3d54886b36f19fcf8e95d3685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7917f6e33cf000627ecf22204d78c08c",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "211fe02228d7f219c2b5",
+    "url": "/FaceRecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ec5054c1201f86a875a9",
+    "url": "/FaceRecognition/static/css/main.cd5d5c02.chunk.css"
+  },
+  {
+    "revision": "211fe02228d7f219c2b5",
+    "url": "/FaceRecognition/static/js/2.f5f9a1ee.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceRecognition/static/js/2.f5f9a1ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5054c1201f86a875a9",
+    "url": "/FaceRecognition/static/js/main.a4ca7f6d.chunk.js"
+  },
+  {
+    "revision": "fe111d7312de8cc63f32",
+    "url": "/FaceRecognition/static/js/runtime-main.70cbb673.js"
+  }
+]);
